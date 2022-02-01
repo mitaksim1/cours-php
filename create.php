a5c6fac82c6c6b6199cd02b95051f643cf681b67
Mimi GIT <miriamt.simonnet@gmail.com>
2022-02-01 11:42:33 +0100

    Validations des inputs : stockage des erreurs dans un tableau + message d'erreur en cas des données manquantes

--- create.php
+++ create.php
@@ -5,7 +5,9 @@ $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'mimi', 'M
 // Si problème avec la connexion, cette config va nous retourner un message d'erreur
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo $_SERVER['REQUEST_METHOD'] . '<br>';
+// Pour capturer les erreurs, on crée une variable du type array qui va les stocker d'abord
+$errors = [];
+
 // On récupère les informations passés dans le formulaire grâce à $_POST
 // On vérifie d'abord si les informations envoyées sont envoyées avec la méthode POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -15,17 +17,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Pour récupérer la date du jour, on appelle date() en lui passant le format attendue par MySQL
     $date = date('Y-m-d H:i:s');
 
-    // On crée des variables pour plus de sécurité, ça empêche de recevoir des injections SQL
-    $statement = $pdo->prepare("INSERT INTO products (title, image, description, price, create_date)
-                VALUES (:title, :image, :description, :price, :date)");
 
-    // On précise à PDO à quoi correspondent les variables créées
-    $statement->bindValue(':title', $title);
-    $statement->bindValue(':image', '');
-    $statement->bindValue(':description', $description);
-    $statement->bindValue(':price', $price);
-    $statement->bindValue(':date', $date);
-    $statement->execute();
+    // Validations
+    if (!$title) {
+      $errors[] = 'Product title is required';
+    }
+    if (!$price) {
+      $errors[] = 'Product price is required';
+    }
+
+    // On n'enregistre les données que s'il n'y a pas des erreurs
+    if (empty($errors)) {
+      // On crée des variables pour plus de sécurité, ça empêche de recevoir des injections SQL
+      $statement = $pdo->prepare("INSERT INTO products (title, image, description, price, create_date)
+                                   VALUES (:title, :image, :description, :price, :date)");
+                                   
+      // On précise à PDO à quoi correspondent les variables créées
+      $statement->bindValue(':title', $title);
+      $statement->bindValue(':image', '');
+      $statement->bindValue(':description', $description);
+      $statement->bindValue(':price', $price);
+      $statement->bindValue(':date', $date);
+      $statement->execute();
+    }    
 }
 ?>
 
@@ -42,9 +56,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Products CRUD</title>
   </head>
   <body>    
-
+    <!-- FORM -->
     <div class="container">
         <h1>Create new product</h1>
+
+        <!-- Message d'erreur si donnée manquante  -->
+        <?php if (!empty($errors)): ?>
+            <div class="alert alert-danger">
+              <?php foreach ($errors as $error): ?>
+                <div><?php echo $error ?></div>
+              <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
+        
         <form action="" method="post">
           <div class="form-group mb-3">
             <label>Product Image</label>
@@ -64,13 +88,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
           <div class="form-group mb-3">
             <label>Product Price</label>
-            <input type="number" name="price" step=".01" class="form-control">
+            <input type="number" name="price" step="0.1" class="form-control">
           </div>
 
           <button type="submit" class="btn btn-primary">Submit</button>
         </form>
-    </div>
-    
+    </div>  
   </body>
 </html>
 
